0b5cc7c125ff784bd355b9a5d750f7dff8fe9b90
Tony Hoang <tonyh@hydrabyte.co>
2026-03-05 21:22:12 +0700

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'drive-simulation',
+      script: 'server/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '256M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      env_development: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+    },
+  ],
+};
